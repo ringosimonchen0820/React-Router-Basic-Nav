52b0fb3ae56bf8fd9e129bae84912523373efea3
SimonChen <ringosimonchen0820@gmail.com>
2020-01-28 15:13:58 -0500

    initial commit and setup routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-
+import { Route, Switch } from 'react-router-dom';
 const App = () => (
   <div>
-    <Navigation />
+    <div>
+      <Navigation />
+      <Switch>
+        <Route exact path='/'><Home /></Route>
+        <Route path='/about'><About /></Route>
+        <Route path='/contact'><Contact /></Route>
+      </Switch>
+    </div>
   </div>
 );
 
