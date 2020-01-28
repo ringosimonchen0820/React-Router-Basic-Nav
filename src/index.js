15d6c10f798642a23b855bcb92e503bf9560374e
SimonChen <ringosimonchen0820@gmail.com>
2020-01-28 15:29:51 -0500

    set up links

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowerRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
     <Router>
